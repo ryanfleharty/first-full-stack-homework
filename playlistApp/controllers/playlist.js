89126fc987f560af26450c84452c58e8de3d22d3
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-04-18 08:46:25 -0600

    fixed syntax error

--- playlistApp/controllers/playlist.js
+++ playlistApp/controllers/playlist.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Playlist = require('../models/playlist');
 
+//index
 router.get('/',(req, res)=>{
     Playlist.find({}, (error, wholePlaylist) => {
 
@@ -15,12 +16,14 @@ router.get('/',(req, res)=>{
     })
 });
 
+//create
 router.get('/new',(req, res)=>{
     res.render('new.ejs',{
         playlist:Playlist
     });
 });
 
+//update
 router.post('/', (req, res) => {
     Playlist.create(req.body, (err, playlistItem) => {
         if (err) {
@@ -31,7 +34,26 @@ router.post('/', (req, res) => {
     })
 });
 
-router.get('id:/edit',(req, res)=>{
-    res.send('edit route hit')
+//edit
+router.get('/id:/edit',(req, res)=>{
+    res.render('edit.ejs', ({
+        id: req.params.id
+    }))
 });
+
+//show
+router.get('/id:',(req, res)=>{
+    Playlist.findOne(req.params.id, (err, playlistItem) => {
+    if (err) {
+        res.send(err);
+    } else {
+        res.redirect('show.ejs', {
+            playlist: playlistItem
+        });
+    }
+    console.log(playlistItem)
+    })
+    });
+    
+
 module.exports = router;

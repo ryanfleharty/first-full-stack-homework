3ce888003d526c3b1bc8318436af91a498f7dbbf
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-04-18 06:26:57 -0600

    index list works with testdb

--- playlistApp/controllers/playlist.js
+++ playlistApp/controllers/playlist.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
-//const Playlist = require('../models/playlist');
+const Playlist = require('../models/playlist');
 
 router.get('/',(req, res)=>{
-    res.send('index route hit');
+    res.render('index.ejs', {
+        playlist: Playlist
+    })
 });
 
 router.get('/new',(req, res)=>{
     res.send('new route hit')
 });
 
+router.post('/',(req, res)=>{
+    res.send('create route hit')
+})
 
+router.get('id:/edit',(req, res)=>{
+    res.send('edit route hit')
+});
 module.exports = router;
--- playlistApp/models/playlist.js
+++ playlistApp/models/playlist.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose'); //require mongoose package
-const Schema = mongoose.Schema; //mongoose has many properties on it.  One is a constructor function for Schemas
+// const mongoose = require('mongoose'); //require mongoose package
+// const Schema = mongoose.Schema; //mongoose has many properties on it.  One is a constructor function for Schemas
 
-const playlistSchema = new Schema({
-    song: {
-        type: String,
-        required: true,
-        unique: true
-    }, 
-    artist: {
-        type: String,
-        required: true
-    },
-});
+// const playlistSchema = new Schema({
+//     song: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     }, 
+//     artist: {
+//         type: String,
+//         required: true
+//     },
+// });
 
 //Creating an playlist class -- will be stored in 'playlists' collection.  Mongo does this for you automatically
-const playlist = mongoose.model('playlist', playlistSchema);
+//const Playlist = mongoose.model('playlist', playlistSchema);
 
+const Playlist = [{song:"Sunglasses at Night", artist: "Eddie Money"}, {song:"Never Gonna Give You Up", artist: "Rick Astley"}]
 module.exports = Playlist;

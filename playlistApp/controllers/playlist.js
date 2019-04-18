c53138b96b45d431a758bdefc0cd61ecfc855328
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-04-18 05:32:44 -0600

    server is listening and mongoose is connected to mongo

---
+++ playlistApp/controllers/playlist.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const router = express.Router();
+// const Fruit = require('../models/fruits');
+
+
+
+
+
+// module.exports = router;
---
+++ playlistApp/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+// require our db file
+require('./db/db');
+
+// Require our controller
+//const playlistController = require('./controllers/playlist');
+
+
+
+
+
+
+// middleWare
+// We want to make sure that we set this up
+// before our routes, because we want the request's body
+// to parsed (readable, won't be undefined anymore)
+
+// middleware is set up with the method app.use()
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(methodOverride('_method'))
+// app.use(function(req, res, next) {
+
+// })
+// First argument, is where we want all the routes in the controller (2nd arguemnt),
+// to live at, So every single rout in playlistContoller /playlist
+//app.use('/playlist', playlistController);
+
+
+app.listen(3000, () => {
+    console.log('app listening on port: ', 3000);
+});

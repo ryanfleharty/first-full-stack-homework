2751ece0d5ef54aedf3902891aea2f94c4333ed9
Destiny <dshillis1@gmail.com>
2019-04-17 22:50:21 -0600

    Create Route Setup

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -14,7 +14,32 @@ router.get('/', (req, res)=> {
             })
         }
     })
-})
+});
+
+// create
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+router.post('/', (req, res) => {
+    if (req.body.healthy === 'on'){
+        req.body.healthy = true;
+    } else {
+        req.body.healthy = false;
+    }
+    if (req.body.comfort === 'on'){
+        req.body.comfort = true;
+    } else {
+        req.body.comfort = false;
+    }
+    Food.create(req.body, (err, newFood) => {
+        if (err) {
+            res.send(err);
+        } else (
+            res.redirect('/foodapp')
+        )
+    })
+});
 
 // show
 router.get('/:id', (req, res) => {
@@ -27,10 +52,8 @@ router.get('/:id', (req, res) => {
             })
         }
     })
-})
+});
 
-
-// create
 
 // edit
 
--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use(morgan('short'));
 app.use('/foodapp', foodController);
+app.use(express.static('public'));
 
 
 const port = 3000;
 app.listen(port, () => {
     console.log('Server is listening');
-})
+});

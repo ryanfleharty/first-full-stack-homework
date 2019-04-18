574468ad5a323590a0092b5dc1824ef5e555adc8
Destiny <dshillis1@gmail.com>
2019-04-17 22:01:28 -0600

    Index Created

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -2,4 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Food = require('../models/food');
 
+
+// index
+router.get('/', (req, res)=> {
+    Food.find({}, (err, foods) => {
+        if (err){
+            res.send(err);
+        } else {
+            res.render('index.ejs', {
+                'foods': foods,
+            })
+        }
+    })
+})
+
+// show
+
+// create
+
+// edit
+
+// delete
+
 module.exports = router;

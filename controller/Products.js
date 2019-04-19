6b5a58d55193c94499c04c4559660af3c54deb86
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-19 08:57:33 -0600

    added more routes

--- controller/Products.js
+++ controller/Products.js
@@ -34,7 +34,7 @@ router.post('/', (req, res) => {
 
 // Edit products
 router.put('/:id', (req, res) => {
-    product.findByIdAndUpdate(req.params.id, req.body, (err, updateProducts) => {
+    product.findByIdAndUpdate(req.params.id, req.body, (err, updateProduct) => {
         if (err) {
             console.log(err);
             res.send(err);
@@ -43,6 +43,46 @@ router.put('/:id', (req, res) => {
         }
     })
 });
+
+router.get('/:id/edit', (req, res) => {
+    product.findByID(req.params.id, (err, foundProduct) =>{
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(findProduct);
+            res.render('edit.ejs', {products: foundProduct});
+        }
+    })
+}); 
+
+// Delete products
+router.put('/:id', (req, res) =>{
+    product.findByIDAndRemove(req.params.id, req.body, (err, deletedProduct) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            console.log(deletedProduct);
+            res.redirect('index.ejs');
+        }
+    })
+});
+
+
+//Show products
+router.get('/:id/edit', (req, res) => {
+    product.findByID(req.params.id, (err, foundProduct) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            console.log(findProduct);
+            res.render('show.ejs', { products: foundProduct });
+        }
+    })
+}); 
+
+
 
 
 

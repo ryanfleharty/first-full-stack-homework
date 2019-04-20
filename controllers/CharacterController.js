beeafefd86c4f9856bed5f604e7ead68a27c0be9
Nick Johnson <Blackjacknick@gmail.com>
2019-04-20 13:03:27 -0600

    Create Route added

--- controllers/CharacterController.js
+++ controllers/CharacterController.js
@@ -2,23 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Character = require('../models/Characters');
 
+
+// Index route
 router.get('/', (req, res)=>{
-    // find all of the cats, stored in a variable called catsFromTheDatabase
     Character.find({}, (err, charsFromDB)=>{
-        // render the index page 
         res.render('../views/index.ejs', {
-        // names a variable to be displayed on the index.ejs
             Characters: charsFromDB
         })
     })
 })
 
+// New Route
 router.get('/new', (req, res) =>{
 	res.render('../views/new.ejs')
 })
 
 
+// Create Route
 
+router.post('/new', (req, res)=>{
+	          if(req.body.alive === 'on'){
+    req.body.alive = true;
+  } else {
+    req.body.alive = false
+  }
+    Character.create(req.body, (error, createdChar)=> {
+        if (error){
+            console.log(error)
+        } else {
+
+            console.log(createdChar);
+            res.redirect('/');
+        }
+    })
+});
 
 
 

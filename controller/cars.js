0e056b513d0cfed4cb9e207f021dc396be2ba915
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-04-22 20:09:47 -0600

    full stack done

--- controller/cars.js
+++ controller/cars.js
@@ -2,58 +2,40 @@ const express = require('express')
 const router = express.Router()
 const Car = require('../models/car')
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const carFound = await Car.find({})
-//     res.render('index.ejs', {
-//       cars: CarsDB
-//     })
-//   } catch(err) {
-//     res.send(err)
-//   }
-// })
 router.get('/', (req, res) => {
   Car.find({}, (error, CarsDB) => {
     if(error) {
       res.send(error)
       console.log(error);
     } else {
-      res.render('index.ejs', {cars: CarsDB})
+      res.render('index.ejs', {cars: carFound})
     }
   })
 })
-router.get('/cars:id', (req, res) => {
-  res.render('show.ejs', {cars: CarsDB[req.params.id]})
-})
-router.post('/', async (req, res) => {
-  try {
-    const carCreated = await Car.create(req.body)
-    res.redirect('/cars')
-  } catch(err) {
-    res.send(err)
-  }
-})
-// router.post('/', (req, res) => {
-//   console.log(req.body);
-//   Car.create(req.body, (err, createdCar) => {
-//   if(err) {
-//     res.send(err)
-//   } else {
-//     res.redirect('/cars')
-//     }
-//   })
-// })
-router.get('/new', async (req, res) => {
-  try {
-    res.render('new.ejs')
-  } catch (err) {
-    res.send(err)
-  }
-})
-// router.get('/new', (req, res) => {
-//   res.render('new.ejs')
-// })
 
+router.post('/', (req, res) => {
+  // console.log(req.body);
+  // if(req.body === null){
+  //   req.body = true
+  // } else {
+  //   req.body = false
+  // }
+  Car.create(req.body, (err, createdCar) => {
+  if(err) {
+    res.send(err)
+  } else {
+    res.redirect('/cars')
+    }
+  })
+})
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
+})
+router.put('/:id', (req, res) => {
+  Car.findByIdAndUpdate(req.params.id, req.body, (err, updateCar) => {
+      res.redirect('/cars')
+  })
+})
 router.delete('/:id', (req, res) => {
   Car.findByIdAndDelete(req.params.id, (err, deleteCar) => {
     if(err) {
@@ -63,6 +45,15 @@ router.delete('/:id', (req, res) => {
     }
   })
   res.redirect('/cars')
+})
+router.get('/cars:id', (req, res) => {
+  Car.findById(req.params.id, (err, foundCar) => {
+    if(err) {
+      res.send(err)
+    } else {
+      res.render('show.ejs', {cars: carFound[req.params.id]})
+    }
+  })
 })
 
 
